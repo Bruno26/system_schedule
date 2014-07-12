d416c02fd84ecf18ecd1700d8107bcf5dda5091e
Bruno-Palacios <bruno-palacios@Bruno.cantv.net>
2014-07-11 22:45:54 -0430

    sin merge

--- protected/models/VswCarreraMaterias.php
+++ protected/models/VswCarreraMaterias.php
@@ -17,118 +17,110 @@
  * @property string $str_corto_materia
  * @property boolean $es_activo
  */
-class VswCarreraMaterias extends CActiveRecord
-{
-	/**
-	 * @return string the associated database table name
-	 */
-	public function tableName()
-	{
-		return 'vsw_carrera_materias';
-	}
-        
-	/**
-	 * primary key
-	 */
-	public function primaryKey()
-	{
-		return 'id_materia';
-	}
-        
-        
+class VswCarreraMaterias extends CActiveRecord {
 
-	/**
-	 * @return array validation rules for model attributes.
-	 */
-	public function rules()
-	{
-		// NOTE: you should only define rules for those attributes that
-		// will receive user inputs.
-		return array(
-			array('id_materia, fk_carrera, fk_trayecto, fk_trimestre, id_seccion, seccion', 'numerical', 'integerOnly'=>true),
-			array('carrera, trayecto, trimestre, str_materia, str_corto_materia, es_activo', 'safe'),
-			// The following rule is used by search().
-			// @todo Please remove those attributes that should not be searched.
-			array('id_materia, fk_carrera, carrera, fk_trayecto, trayecto, fk_trimestre, trimestre, id_seccion, seccion, str_materia, str_corto_materia, es_activo', 'safe', 'on'=>'search'),
-		);
-	}
+    /**
+     * @return string the associated database table name
+     */
+    public function tableName() {
+        return 'vsw_carrera_materias';
+    }
 
-	/**
-	 * @return array relational rules.
-	 */
-	public function relations()
-	{
-		// NOTE: you may need to adjust the relation name and the related
-		// class name for the relations automatically generated below.
-		return array(
-		);
-	}
+    /**
+     * primary key
+     */
+    public function primaryKey() {
+        return 'id_materia';
+    }
 
-	/**
-	 * @return array customized attribute labels (name=>label)
-	 */
-	public function attributeLabels()
-	{
-		return array(
-			'id_materia' => 'Id Materia',
-			'fk_carrera' => 'Fk Carrera',
-			'carrera' => 'Carrera',
-			'fk_trayecto' => 'Fk Trayecto',
-			'trayecto' => 'Trayecto',
-			'fk_trimestre' => 'Fk Trimestre',
-			'trimestre' => 'Trimestre',
-			'id_seccion' => 'Id Seccion',
-			'seccion' => 'Seccion',
-			'str_materia' => 'Materia',
-			'str_corto_materia' => 'Abreviatura',
-			'es_activo' => 'Es Activo',
-		);
-	}
+    /**
+     * @return array validation rules for model attributes.
+     */
+    public function rules() {
+        // NOTE: you should only define rules for those attributes that
+        // will receive user inputs.
+        return array(
+            array('id_materia, fk_carrera, fk_trayecto, fk_trimestre, id_seccion, seccion', 'numerical', 'integerOnly' => true),
+            array('carrera, trayecto, trimestre, str_materia, str_corto_materia, es_activo', 'safe'),
+            // The following rule is used by search().
+            // @todo Please remove those attributes that should not be searched.
+            array('id_materia, fk_carrera, carrera, fk_trayecto, trayecto, fk_trimestre, trimestre, id_seccion, seccion, str_materia, str_corto_materia, es_activo', 'safe', 'on' => 'search'),
+        );
+    }
 
-	/**
-	 * Retrieves a list of models based on the current search/filter conditions.
-	 *
-	 * Typical usecase:
-	 * - Initialize the model fields with values from filter form.
-	 * - Execute this method to get CActiveDataProvider instance which will filter
-	 * models according to data in model fields.
-	 * - Pass data provider to CGridView, CListView or any similar widget.
-	 *
-	 * @return CActiveDataProvider the data provider that can return the models
-	 * based on the search/filter conditions.
-	 */
-	public function search()
-	{
-		// @todo Please modify the following code to remove attributes that should not be searched.
+    /**
+     * @return array relational rules.
+     */
+    public function relations() {
+        // NOTE: you may need to adjust the relation name and the related
+        // class name for the relations automatically generated below.
+        return array(
+        );
+    }
 
-		$criteria=new CDbCriteria;
+    /**
+     * @return array customized attribute labels (name=>label)
+     */
+    public function attributeLabels() {
+        return array(
+            'id_materia' => 'Id Materia',
+            'fk_carrera' => 'Fk Carrera',
+            'carrera' => 'Carrera',
+            'fk_trayecto' => 'Fk Trayecto',
+            'trayecto' => 'Trayecto',
+            'fk_trimestre' => 'Fk Trimestre',
+            'trimestre' => 'Trimestre',
+            'id_seccion' => 'Id Seccion',
+            'seccion' => 'Seccion',
+            'str_materia' => 'Materia',
+            'str_corto_materia' => 'Abreviatura',
+            'es_activo' => 'Es Activo',
+        );
+    }
 
-		$criteria->compare('id_materia',$this->id_materia);
-		$criteria->compare('fk_carrera',$this->fk_carrera);
-		$criteria->compare('carrera',$this->carrera,true);
-		$criteria->compare('fk_trayecto',$this->fk_trayecto);
-		$criteria->compare('trayecto',$this->trayecto,true);
-		$criteria->compare('fk_trimestre',$this->fk_trimestre);
-		$criteria->compare('trimestre',$this->trimestre,true);
-		$criteria->compare('id_seccion',$this->id_seccion);
-		$criteria->compare('seccion',$this->seccion);
-		$criteria->compare('str_materia',$this->str_materia,true);
-		$criteria->compare('str_corto_materia',$this->str_corto_materia,true);
-		$criteria->compare('es_activo',$this->es_activo);
+    /**
+     * Retrieves a list of models based on the current search/filter conditions.
+     *
+     * Typical usecase:
+     * - Initialize the model fields with values from filter form.
+     * - Execute this method to get CActiveDataProvider instance which will filter
+     * models according to data in model fields.
+     * - Pass data provider to CGridView, CListView or any similar widget.
+     *
+     * @return CActiveDataProvider the data provider that can return the models
+     * based on the search/filter conditions.
+     */
+    public function search() {
+        // @todo Please modify the following code to remove attributes that should not be searched.
 
-		return new CActiveDataProvider($this, array(
-			'criteria'=>$criteria,
-		));
-	}
+        $criteria = new CDbCriteria;
 
-	/**
-	 * Returns the static model of the specified AR class.
-	 * Please note that you should have this exact method in all your CActiveRecord descendants!
-	 * @param string $className active record class name.
-	 * @return VswCarreraMaterias the static model class
-	 */
-	public static function model($className=__CLASS__)
-	{
-		return parent::model($className);
-	}
+        $criteria->compare('id_materia', $this->id_materia);
+        $criteria->compare('fk_carrera', $this->fk_carrera);
+        $criteria->compare('carrera', $this->carrera, true);
+        $criteria->compare('fk_trayecto', $this->fk_trayecto);
+        $criteria->compare('trayecto', $this->trayecto, true);
+        $criteria->compare('fk_trimestre', $this->fk_trimestre);
+        $criteria->compare('trimestre', $this->trimestre, true);
+        $criteria->compare('id_seccion', $this->id_seccion);
+        $criteria->compare('seccion', $this->seccion);
+        $criteria->compare('str_materia', $this->str_materia, true);
+        $criteria->compare('str_corto_materia', $this->str_corto_materia, true);
+        $criteria->compare('es_activo', $this->es_activo);
+
+        return new CActiveDataProvider($this, array(
+            'criteria' => $criteria,
+        ));
+    }
+
+    /**
+     * Returns the static model of the specified AR class.
+     * Please note that you should have this exact method in all your CActiveRecord descendants!
+     * @param string $className active record class name.
+     * @return VswCarreraMaterias the static model class
+     */
+    public static function model($className = __CLASS__) {
+        return parent::model($className);
+    }
+
 }
