685ad66bdcf7f863b5ca53b315d86433cc96a700
Bruno-Palacios <bruno-palacios@Bruno.cantv.net>
2014-07-12 00:07:29 -0430

    layouts

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -29,9 +29,9 @@
 	<div id="mainmenu">
 		<?php $this->widget('zii.widgets.CMenu',array(
 			'items'=>array(
-				array('label'=>'Home', 'url'=>array('/site/index')),
-				array('label'=>'About', 'url'=>array('/site/page', 'view'=>'about')),
-				array('label'=>'Contact', 'url'=>array('/site/contact')),
+				array('label'=>'Inicio', 'url'=>array('/site/index')),
+//				array('label'=>'About', 'url'=>array('/site/page', 'view'=>'about')),
+//				array('label'=>'Contact', 'url'=>array('/site/contact')),
 				array('label'=>'Aula', 'url'=>array('/aula/admin')),
 				array('label'=>'Sección', 'url'=>array('/seccion/admin')),
 				array('label'=>'Materia', 'url'=>array('/materia/admin')),
