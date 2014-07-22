64d791eebfac29a16cbf2c7affe7f5fb02e049f6
Bruno Palacios <bruno.palacios26@gmail.com>
2014-07-21 22:40:53 -0430

    footer

--- themes/abound/views/layouts/tpl_footer.php
+++ themes/abound/views/layouts/tpl_footer.php
@@ -2,7 +2,7 @@
         <div class="subnav navbar navbar-fixed-bottom">
             <div class="navbar-inner">
                 <div class="container">
-                    Creado por: Edwing Díaz -  Proyecto Socio-Tecnológico.
+                    Creado por: Edwim Díaz.
                 </div>
             </div>
         </div>      
